6dcfab12d20b404da8fbd83fce00a0732c2b6830
Tomasz Kwiatkowski <t.kwiatkowski@fri.edu.pl>
2024-11-28 10:09:27 +0100

    Package method declarations, Simple test

--- src/sio3pack/packages/package/model.py
+++ src/sio3pack/packages/package/model.py
@@ -1,6 +1,47 @@
+from typing import Any
+
+from sio3pack.files.file import File
+from sio3pack.graph.graph import Graph
+from sio3pack.test.test import Test
+
+
 class Package:
     """
     Base class for all packages.
     """
 
-    pass
+    def __init__(self):
+        pass
+
+    def get_task_id(self) -> str:
+        pass
+
+    def get_titles(self) -> dict[str, str]:
+        pass
+
+    def get_title(self, lang: str | None = None) -> str:
+        pass
+
+    def get_statements(self) -> dict[str, File]:
+        pass
+
+    def get_statement(self, lang: str | None = None) -> File:
+        pass
+
+    def get_config(self) -> dict[str, Any]:
+        pass
+
+    def get_tests(self) -> list[Test]:
+        pass
+
+    def get_test(self, test_id: str) -> Test:
+        pass
+
+    def get_package_graph(self) -> Graph:
+        pass
+
+    def get_run_graph(self, file: File, tests: list[Test] | None = None) -> Graph:
+        pass
+
+    def get_save_outs_graph(self, tests: list[Test] | None = None) -> Graph:
+        pass
---
+++ src/sio3pack/test/simple_test.py
@@ -0,0 +1,23 @@
+from sio3pack.files.file import File
+from sio3pack.test.test import Test
+
+
+class SimpleTest(Test):
+    def __init__(
+        self,
+        file_in: File,
+        file_out: File,
+        group: str,
+        default_mem_limit: int,
+        default_time_limit: int,
+        mem_limits: dict[str, int],
+        time_limits: dict[str, int],
+    ):
+        super().__init__()
+        self.file_in = file_in
+        self.file_out = file_out
+        self.group = group
+        self.default_mem_limit = default_mem_limit
+        self.default_time_limit = default_time_limit
+        self.mem_limits = mem_limits
+        self.time_limits = time_limits
