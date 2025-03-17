fd3e47bf198696a6e72e954ab0384cb773c2732f
Tomasz Kwiatkowski <t.kwiatkowski@fri.edu.pl>
2025-03-17 19:22:43 +0100

    Sphinx docs using AutoAPI

---
+++ docs/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'SIO3Pack'
+copyright = '2025, Tomasz Kwiatkowski, Mateusz Masiarz, Jakub Rożek, Stanisław Struzik'
+author = 'Tomasz Kwiatkowski, Mateusz Masiarz, Jakub Rożek, Stanisław Struzik'
+release = '1.0.0.dev1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'autoapi.extension',
+    'sphinx.ext.autodoc',  # Also required by AutoAPI.
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+autoapi_dirs = ['../src/sio3pack/']
+
+def should_skip_submodule(app, what, name, obj, skip, options):
+    if what == "module":
+        skip = True
+
+    submodule = name.split(".")[-1]
+    if submodule in ["migrations"]:
+        skip = True
+    return skip
+
+def setup(sphinx):
+    sphinx.connect("autoapi-skip-member", should_skip_submodule)
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
