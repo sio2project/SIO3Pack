2d656279270cfd83b875477c0f6d86e19f413aa4
Tomasz Kwiatkowski <t.kwiatkowski@fri.edu.pl>
2025-05-05 10:36:01 +0200

    Fix constructor

--- src/sio3pack/test/test.py
+++ src/sio3pack/test/test.py
@@ -7,7 +7,7 @@ class Test:
     """
 
     def __init__(self, test_name: str, test_id: str, in_file: File, out_file: File, group: str):
-        self.test_name = self.test_name
+        self.test_name = test_name
         self.test_id = test_id
         self.in_file = in_file
         self.out_file = out_file
